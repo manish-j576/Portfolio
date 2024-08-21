cbbc813752349bc80d7e8a3b7dd9cee8c9c41481
Manish Joshi <manishjoshi705572@gmail.com>
2024-08-21 22:28:03 +0530

    first and second section complte

---
+++ script.js
@@ -0,0 +1,14 @@
+function openSocial(social) {
+  if(social==="LINKEDIN"){
+    window.open("https://www.linkedin.com/in/manish-joshi-36491a247", "_blank");
+  }
+  else if(social==="TWITTER"){
+    window.open("https://x.com/ManishJ76124569", "_blank");
+
+  }
+  else{
+    window.open("https://github.com/manish-j576?tab=repositories", "_blank");
+
+  }
+}
+console.log("Script started..");
